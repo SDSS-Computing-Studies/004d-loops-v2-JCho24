7074f497af57dd996f6ac1a5bf831fd688896adb
JCho24 <johnc415@deltalearns.ca>
2020-10-02 11:35:00 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -3,9 +3,10 @@
 a = input("Enter a number: ").strip()
 a = int(a)
 t = a
-h = a + 1
-for i in range(1,h):
+h = h + 1
+for i in (1,h):
     a = a - 1
+    print(a)
     b = t * a 
     c = b * a
     print(c)
