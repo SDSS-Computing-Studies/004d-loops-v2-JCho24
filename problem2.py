14a43d481f108fdfb0c75276f185bfc2ac97d039
JCho24 <johnc415@deltalearns.ca>
2020-10-02 12:24:33 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -2,11 +2,10 @@
 
 a = input("Enter a number: ").strip()
 a = int(a)
-t = a
-h = h + 1
-for i in (1,h):
-    a = a - 1
-    print(a)
-    b = t * a 
-    c = b * a
-    print(c)
+b = 0
+c = 1
+for i in range(0,a):
+    b = a - i
+    c = b * c
+
+print(str(a)+"! is "+str(c) +" where "+str(a)+" is the integer entered and " +str(c)+ " is the calculated answer")
